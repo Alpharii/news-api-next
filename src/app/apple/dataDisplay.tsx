d0e89a1d8c04b683fb861a13de4b6fa2803fbafd
alpharii <alphari404@gmail.com>
2024-10-03 23:25:59 +0700

    add style

--- src/app/apple/dataDisplay.tsx
+++ src/app/apple/dataDisplay.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { getApple } from '../services/Apple';
+import { getNews } from '../services/news';
 
 interface Article {
     title: string;
@@ -18,7 +18,7 @@ export default function DataDisplay() {
     useEffect(() => {
         const loadData = async () => {
             try {
-                const result = await getApple();
+                const result = await getNews();
                 setData(result.articles); // Pastikan data diatur ke array articles
             } catch (err) {
                 if (err instanceof Error) {
@@ -38,21 +38,19 @@ export default function DataDisplay() {
     if (error) return <p>Error: {error}</p>;
 
     return (
-        <div className=''>
-            {data.length > 0 ? (
-                <ul>
-                    {data.map((article, index) => (
-                        <li key={index}>
-                            <h2>{article.title}</h2>
-                            {article.urlToImage && <img src={article.urlToImage} alt={article.title} />}
-                            <p>{article.description}</p>
-                            <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No articles found.</p>
-            )}
-        </div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-6'>
+        {data.length > 0 ? (
+            data.map((article, index) => (
+                <div key={index} className='max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4'>
+                    <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>{article.title}</h2>
+                    {article.urlToImage && <img src={article.urlToImage} alt={article.title} className='rounded-t-lg' />}
+                    <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>{article.description}</p>
+                    <a href={article.url} target="_blank" rel="noopener noreferrer" className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Read more</a>
+                </div>
+            ))
+        ) : (
+            <p>No articles found.</p>
+        )}
+    </div>
     );
 }
--- src/app/services/news.ts
+++ src/app/services/news.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const API_URL = `https://newsapi.org/v2/everything?q=apple&from=2024-10-02&to=2024-10-02&sortBy=popularity&apiKey=ed4f05aca2b04539875512d8936b57a2`; //keynya jangan di ambil bg :v
+const APPLE_API_URL = `https://newsapi.org/v2/everything?q=apple&from=2024-10-02&to=2024-10-02&sortBy=popularity&apiKey=ed4f05aca2b04539875512d8936b57a2`; //keynya jangan di ambil bg :v
 
-export const getApple = async () => {
+export const getNews = async () => {
     try {
-        const response = await axios.get(API_URL);
+        const response = await axios.get(APPLE_API_URL);
         return response.data;
     } catch (error) {
         if (axios.isAxiosError(error)) {
